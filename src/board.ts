8ce3645ae5a1f41cd50f334692d60b3303cec117
dbustanUCSC <132178390+dbustanUCSC@users.noreply.github.com>
2023-11-10 10:11:54 -0800

    changes to all

--- src/board.ts
+++ src/board.ts
@@ -1,4 +1,15 @@
-/*import { MERRILL_CLASSROOM } from "./main";
+import leaflet from "leaflet";
+import { MERRILL_CLASSROOM } from "./main";
+
+const TILE_DEGREES = 1e-4;
+
+
+interface Cell {
+    readonly i: number;
+    readonly j: number;
+}
+
+
 export class Board {
 
     readonly tileWidth: number;
@@ -7,30 +18,69 @@ export class Board {
     private readonly knownCells: Map<string, Cell>;
 
     constructor(tileWidth: number, tileVisibilityRadius: number) {
-        // ...
+        this.tileWidth = tileWidth;
+        this.tileVisibilityRadius = tileVisibilityRadius;
+        this.knownCells = new Map();
     }
-
-    private getCanonicalCell(cell: Cell): Cell {
+   
+    private getCanonicalCell(cell: Cell): Cell | undefined {
         const { i, j } = cell;
         const key = [i, j].toString();
-        // ...
+        if (!this.knownCells.has(key)) {
+            console.log("no cell exists");
+            return undefined;
+        }
         return this.knownCells.get(key)!;
     }
-
-    getCellForPoint(point: leaflet.LatLng): Cell {
-        return this.getCanonicalCell({
-            // ...
-        });
+    createCanonicalCell(i: number, j: number): Cell | undefined {
+        const cell: Cell = {i: i, j: j};
+        const key = [i, j].toString();
+        if (this.knownCells.has(key)) {
+            console.log("there is already a cell located there.");
+            return undefined;
+        } else {
+            this.knownCells.set(key, cell)!;
+            return cell;
+        }
+    }
+    getCellForPoint(point: leaflet.LatLng): Cell | undefined {
+        const i = Math.floor(point.lat/ this.tileWidth);
+        const j = Math.floor(point.lng / this.tileWidth);
+        if (this.getCanonicalCell({ i, j }) != undefined) {
+            return this.getCanonicalCell({i, j})!;
+        }
     }
 
     getCellBounds(cell: Cell): leaflet.LatLngBounds {
-    	// ...
+        const bounds = leaflet.latLngBounds([
+            [
+              MERRILL_CLASSROOM.lat + cell.i * TILE_DEGREES,
+              MERRILL_CLASSROOM.lng + cell.j * TILE_DEGREES,
+            ],
+            [
+              MERRILL_CLASSROOM.lat + (cell.i + 1) * TILE_DEGREES,
+              MERRILL_CLASSROOM.lng + (cell.j + 1) * TILE_DEGREES,
+            ],
+          ]);
+          return bounds;
     }
 
     getCellsNearPoint(point: leaflet.LatLng): Cell[] {
         const resultCells: Cell[] = [];
-        const originCell = this.getCellForPoint(point);
-        // ...
+        
+        const originCell = this.getCellForPoint(point)!;
+
+        for (let i = -this.tileVisibilityRadius; i < this.tileVisibilityRadius; i++) {
+            for (let j = - this.tileVisibilityRadius; j < this.tileVisibilityRadius; j++) {
+                const cellInRadius: Cell = { i: originCell.i + i, j: originCell.j + j };
+                const canonicalCell: Cell | undefined = this.getCanonicalCell(cellInRadius);
+                if (canonicalCell != undefined) {
+                    resultCells.push(canonicalCell);
+                }
+                
+            }
+        }
+        
         return resultCells;
     }
-}*/
+}
--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,26 @@ import "./style.css";
 import leaflet from "leaflet";
 import luck from "./luck";
 import "./leafletWorkaround";
+import { Board } from "./board.ts";
 
-/*interface Coin {
+interface Coin {
     i: number,
     j: number,
     index: number,
-}*/
+}
 
-
-
+interface Cell {
+  readonly i: number;
+  readonly j: number;
+}
 export const MERRILL_CLASSROOM = leaflet.latLng({
     lat: 36.9995,
     lng: -122.0533,
 });
-  
+
+
+const board = new Board(0.0001, 8);
+
 const GAMEPLAY_ZOOM_LEVEL = 19;
 const TILE_DEGREES = 1e-4;
 const NEIGHBORHOOD_SIZE = 8;
@@ -57,51 +63,67 @@ sensorButton.addEventListener("click", () => {
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
 statusPanel.innerHTML = "No coins yet...";
 
+
 function makePit(i: number, j: number) {
-  const bounds = leaflet.latLngBounds([
-    [
-      MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
-      MERRILL_CLASSROOM.lng + j * TILE_DEGREES,
-    ],
-    [
-      MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
-      MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES,
-    ],
-  ]);
-
-  const pit = leaflet.rectangle(bounds) as leaflet.Layer;
-
-  pit.bindPopup(() => {
-    let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
-    const container = document.createElement("div");
-    container.innerHTML = `
-                <div>There is a pit here at "${i},${j}". It has value <span id="value">${value}</span>.</div>
-                <button id="poke">poke</button> </div>
-                <button id="deposit">deposit</button>`;
-    const poke = container.querySelector<HTMLButtonElement>("#poke")!;
-    poke.addEventListener("click", () => {
-      value--;
-      container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-        value.toString();
-      points++;
-      statusPanel.innerHTML = `${points} points accumulated`;
+  const currentCell: Cell | undefined = board.createCanonicalCell(i, j);
+  if (currentCell != undefined) {
+    const coins: Coin[] = [];
+    const bounds = board.getCellBounds(currentCell);
+    const pit = leaflet.rectangle(bounds) as leaflet.Layer;
+   
+    pit.bindPopup(() => {
+      //we want each pit to have a random amount of coins 
+      const numOfCoins = Number((luck(`${currentCell.i}, ${currentCell.j}`) * 10).toFixed(0));
+      for (let k = 0; k < numOfCoins; k++){
+        const newCoin: Coin = { i: currentCell.i, j: currentCell.j, index: k };
+        coins.push(newCoin);
+      }
+      let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+      const container = document.createElement("div");
+      container.innerHTML = `
+                  <div>There is a pit here at "${currentCell.i},${currentCell.j}". It has value <span id="value">${coins.length}</span>.</div>
+                  <button id="poke">poke</button> </div>
+                  <button id="deposit">deposit</button>`;
+      const poke = container.querySelector<HTMLButtonElement>("#poke")!;
+      poke.addEventListener("click", () => {
+        coins.pop();
+        container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+          coins.length.toString();
+        points++;
+        statusPanel.innerHTML = `${points} points accumulated`;
+      });
+      const deposit = container.querySelector<HTMLButtonElement>("#deposit")!;
+      deposit.addEventListener("click", () => {
+        const NO_POINTS = 0;
+        if (points <= NO_POINTS) return;
+        points--;
+        value++;
+        container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+          value.toString();
+        statusPanel.innerHTML =
+          points == NO_POINTS
+            ? `No points yet...`
+            : `${points} points accumulated`;
+      });
+      return container;
     });
-    const deposit = container.querySelector<HTMLButtonElement>("#deposit")!;
-    deposit.addEventListener("click", () => {
-      const NO_POINTS = 0;
-      if (points <= NO_POINTS) return;
-      points--;
-      value++;
-      container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-        value.toString();
-      statusPanel.innerHTML =
-        points == NO_POINTS
-          ? `No points yet...`
-          : `${points} points accumulated`;
-    });
-    return container;
-  });
-  pit.addTo(map);
+    pit.addTo(map);
+  }
+  
+  // const bounds = leaflet.latLngBounds([
+  //   [
+  //     MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
+  //     MERRILL_CLASSROOM.lng + j * TILE_DEGREES,
+  //   ],
+  //   [
+  //     MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
+  //     MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES,
+  //   ],
+  // ]);
+
+  
+
+  
 }
 
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
