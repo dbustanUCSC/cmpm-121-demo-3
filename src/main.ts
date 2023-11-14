4b46a27538734a1159a3b4b12226b79b35c6c20b
dbustanUCSC <132178390+dbustanUCSC@users.noreply.github.com>
2023-11-13 21:18:01 -0800

    finished player moving with arrows.

--- src/main.ts
+++ src/main.ts
@@ -5,28 +5,41 @@ import luck from "./luck";
 import "./leafletWorkaround";
 import { Board } from "./board.ts";
 
-interface Coin {
-  location: Cell;
-  index: number;
+interface Geocoin {
+  mintingLocation: Cell;
+  serialNumber: number;
 }
 
 interface Cell {
   readonly i: number;
   readonly j: number;
 }
+
+class Geocache {
+  coins: Geocoin[];
+  constructor(cell: Cell) {
+    this.coins = [];
+    const numInitialCoins = Math.floor(luck(["intialCoins", cell.i, cell.j].toString()) * 3);
+    for (let i = 0; i < numInitialCoins; i++){
+      this.coins.push({ mintingLocation: cell, serialNumber: i });
+    }
+  }
+  
+}
+
+
+
 export const MERRILL_CLASSROOM = leaflet.latLng({
   lat: 36.9995,
   lng: -122.0533,
 });
 
-const board = new Board(0.0001, 8);
-
+const TILE_DEGREES = 0.0001;
 const GAMEPLAY_ZOOM_LEVEL = 19;
 const PIT_SPAWN_PROBABILITY = 0.1;
-const playerInventory: Coin[] = [];
+const playerInventory: Geocoin[] = [];
 const mapContainer = document.querySelector<HTMLElement>("#map")!;
-
-//const pitMap: Map<string, leaflet.Layer> = new Map<string, leaflet.Layer>();
+const board = new Board(TILE_DEGREES, 8);
 const map = leaflet.map(mapContainer, {
   center: MERRILL_CLASSROOM,
   zoom: GAMEPLAY_ZOOM_LEVEL,
@@ -44,94 +57,161 @@ leaflet
   })
   .addTo(map);
 
-const playerMarker = leaflet.marker(MERRILL_CLASSROOM);
-playerMarker.bindTooltip("That's you!");
-playerMarker.addTo(map);
+const buttonNames = ["north", "south", "west", "east", "sensor"];
+const allButtons: HTMLButtonElement[] = [];
 
-const sensorButton = document.querySelector("#sensor")!;
-sensorButton.addEventListener("click", () => {
-  navigator.geolocation.watchPosition((position) => {
-    playerMarker.setLatLng(
-      leaflet.latLng(position.coords.latitude, position.coords.longitude)
-    );
-    map.setView(playerMarker.getLatLng());
+buttonNames.forEach((buttonName) => {
+  const selector = `#${buttonName}`;
+  const button: HTMLButtonElement = document.querySelector(selector)!;
+  button.addEventListener("click", () => {
+    handleButton(buttonName);
   });
 });
 
-const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
-statusPanel.innerHTML = "No coins yet...";
+function handleButton(name: string) {
+  const playerLatLng = playerMarker.getLatLng();
+  switch (name) {
+    case "north":
+      playerLatLng.lat += TILE_DEGREES;
+      break;
+    case "south":
+      playerLatLng.lat -= TILE_DEGREES;
+      playerMarker.setLatLng(playerLatLng);
+      break;
+    case "east":
+      playerLatLng.lng += TILE_DEGREES;
+      playerMarker.setLatLng(playerLatLng);
+      break;
+    case "west":
+      playerLatLng.lng -= TILE_DEGREES;
+      playerMarker.setLatLng(playerLatLng);
+      break;
+    case "sensor":
+      navigator.geolocation.watchPosition((position) => {
+        playerMarker.setLatLng(
+          leaflet.latLng(position.coords.latitude, position.coords.longitude)
+        );
+        map.setView(playerMarker.getLatLng());
+      });
+      break;
+  }
+  playerMarker.setLatLng(playerLatLng);
+  map.setView(playerMarker.getLatLng());
+  pitFactory();
+}
 
-const allPitData = new Map<Cell, Coin[]>();
+
+function setInitialPlayerPos() {
+  const playerMarker = leaflet.marker(MERRILL_CLASSROOM);
+  playerMarker.bindTooltip("That's you!");
+  playerMarker.addTo(map);
+  return playerMarker;
+}
+
+
+
+
+const allPitData = new Map<Cell, Geocoin[]>();
+
+
+
+
+
+
 function makePit(i: number, j: number) {
   const currentCell: Cell = { i: i, j: j };
-    let coinDiv = document.createElement("div");
-  const bounds = board.getCellBounds(currentCell); 
+  let coinDiv = document.createElement("div");
+  const bounds = board.getCellBounds(currentCell);
   const pit = leaflet.rectangle(bounds) as leaflet.Layer;
-  const coins: Coin[] = [];
-    const numOfCoins = Number(
-      (luck(`${currentCell.i}, ${currentCell.j}`) * 10).toFixed(0)
-    );
-  console.log(currentCell.i, currentCell.j);
-    for (let k = 0; k < numOfCoins; k++) {
-      const newCoin: Coin = { location: currentCell, index: k };
-      coins.push(newCoin);
-    }
-    pit.bindPopup(() => {
-      const NO_COINS = 0;
-      const currentCellCoins = allPitData.get(currentCell)!;
-      const container = document.createElement("div");
-      container.innerHTML = `
+  const coins: Geocoin[] = [];
+  const numOfCoins = Number(
+    (luck(`${currentCell.i}, ${currentCell.j}`) * 10).toFixed(0)
+  );
+  for (let k = 0; k < numOfCoins; k++) {
+    const newCoin: Geocoin = { mintingLocation: currentCell, serialNumber: k };
+    coins.push(newCoin);
+  }
+  pit.bindPopup(() => {
+    const NO_COINS = 0;
+    const currentCellCoins = allPitData.get(currentCell)!;
+    console.log("Current Cell Coins = ", currentCellCoins);
+    const container = document.createElement("div");
+    container.innerHTML = `
                   <div>There is a pit here at "${currentCell.i},${currentCell.j}". It has <span id="value">${coins.length}</span> coins.</div>
                   <button id="poke">take</button> </div>
                   <button id="deposit">deposit</button>`;
-      currentCellCoins.forEach((coin) => {
-        const coinData = `${coin.location.i}:${coin.location.j}#${coin.index}`;
-        coinDiv = document.createElement("div");
-        coinDiv.textContent = coinData;
-        container.appendChild(coinDiv);
-      });
-      const poke = container.querySelector<HTMLButtonElement>("#poke")!;
-      poke.addEventListener("click", () => {
-        if (currentCellCoins.length > NO_COINS) {
-          const coinToCollect: Coin = currentCellCoins.pop()!;
-          playerInventory.push(coinToCollect);
-          container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-            currentCellCoins.length.toString();
-          statusPanel.innerHTML = `${playerInventory.length} coins accumulated`;
-
-          const coinDivToRemove = container.lastChild;
-          if (coinDivToRemove instanceof HTMLDivElement) {
-            container.removeChild(coinDivToRemove);
-          }
-        }
-      });
-      const deposit = container.querySelector<HTMLButtonElement>("#deposit")!;
-      deposit.addEventListener("click", () => {
-        if (playerInventory.length <= NO_COINS) return;
-        const coinToDeposit = playerInventory.pop()!;
-        currentCellCoins.push(coinToDeposit);
-        coinDiv = document.createElement("div");
-        coinDiv.textContent = `${coinToDeposit.location.i}:${coinToDeposit.location.j}#${coinToDeposit.index}`;
-        container.append(coinDiv);
+    currentCellCoins.forEach((coin) => {
+      const coinData = `${coin.mintingLocation.i}:${coin.mintingLocation.j}#${coin.serialNumber}`;
+      coinDiv = document.createElement("div");
+      coinDiv.textContent = coinData;
+      container.appendChild(coinDiv);
+    });
+    const poke = container.querySelector<HTMLButtonElement>("#poke")!;
+    poke.addEventListener("click", () => {
+      if (currentCellCoins.length > NO_COINS) {
+        const coinToCollect: Geocoin = currentCellCoins.pop()!;
+        playerInventory.push(coinToCollect);
         container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
           currentCellCoins.length.toString();
-        statusPanel.innerHTML =
-          playerInventory.length == NO_COINS
-            ? `No coins yet...`
-            : `${playerInventory.length} coins accumulated`;
-      });
+        statusPanel.innerHTML = `${playerInventory.length} coins accumulated`;
 
-      return container;
+        const coinDivToRemove = container.lastChild;
+        if (coinDivToRemove instanceof HTMLDivElement) {
+          container.removeChild(coinDivToRemove);
+        }
+      }
     });
+    const deposit = container.querySelector<HTMLButtonElement>("#deposit")!;
+    deposit.addEventListener("click", () => {
+      if (playerInventory.length <= NO_COINS) return;
+      const coinToDeposit = playerInventory.pop()!;
+      currentCellCoins.push(coinToDeposit);
+      coinDiv = document.createElement("div");
+      coinDiv.textContent = `${coinToDeposit.mintingLocation.i}:${coinToDeposit.mintingLocation.j}#${coinToDeposit.serialNumber}`;
+      container.append(coinDiv);
+      container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+        currentCellCoins.length.toString();
+      statusPanel.innerHTML =
+        playerInventory.length == NO_COINS
+          ? `No coins yet...`
+          : `${playerInventory.length} coins accumulated`;
+    });
+    return container;
+  });
   allPitData.set(currentCell, coins);
   pit.addTo(map);
 }
 
-const currentPosition = playerMarker.getLatLng();
-for (const { i, j } of board.getCellsNearPoint(currentPosition)) {
-  if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
-    makePit(i, j);
+// function pitcheck() {
+//   const currentPosition = playerMarker.getLatLng();
+//   for (const { i, j } of board.getCellsNearPoint(currentPosition)) {
+//     if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
+//       const cell: Cell = { i: i, j: j };
+//       console.log(allPitData.get(cell));
+//     }
+//   }
+// }
+
+
+function pitFactory() {
+  const currentPosition = playerMarker.getLatLng();
+  for (const { i, j } of board.getCellsNearPoint(currentPosition)) {
+    if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
+      makePit(i, j);
+    }
   }
 }
+
+
+
+
+const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
+statusPanel.innerHTML = "No coins yet...";
+const playerMarker = setInitialPlayerPos();
+
+pitFactory();
+
+
+
 
 
